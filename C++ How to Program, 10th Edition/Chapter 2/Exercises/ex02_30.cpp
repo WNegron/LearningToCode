dad180427169e6b68d9f3191d37cff3e2c21cc63
W Negron <48074211+WNegron@users.noreply.github.com>
2021-12-12 04:48:13 -0500

    Update ex02_30.cpp
    fixed spelling

--- C++ How to Program, 10th Edition/Chapter 2/Exercises/ex02_30.cpp
+++ C++ How to Program, 10th Edition/Chapter 2/Exercises/ex02_30.cpp
@@ -5,7 +5,7 @@ Filename: ex02_30.cpp
   Author: Warren Negron
    Email: warren.negron@gmail.com
 
-Note: My answer to exercise 2.30 using only the tools and methods thought in Chapter 2.
+Note: My answer to exercise 2.30 using only the tools and methods taught up to Chapter 2.
 
 Description:
 
