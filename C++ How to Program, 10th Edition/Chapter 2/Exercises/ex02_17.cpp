ff8e98fa263ced998444be8b3b5eee7a418ca89b
W Negron <48074211+WNegron@users.noreply.github.com>
2021-12-11 23:31:45 -0500

    ex02_17.cpp
    My answer to exercise 2.17 using only the tools and methods thought in Chapter 2.

---
+++ C++ How to Program, 10th Edition/Chapter 2/Exercises/ex02_17.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************************
+
+		Filename: ex02_17.cpp
+			  Date: 2021-12-09
+		  Author: Warren Negron
+		   Email: warren.negron@gmail.com
+
+
+Description:
+
+2.17 (Printing) Write a program that prints the characters 1 to 4 on the same line
+with each pair of adjacent characters separated by one space i.e. 12 34. 
+Do this several ways:
+a) Using one statement with two stream insertion operators.
+b) Using one statement with four stream insertion operator.
+c) Using two statements with one stream insertion operator each. 
+
+************************************************************************************ */
+
+#include <iostream> //enables program to perform input and output
+using namespace std; //program uses names from the std namespace
+
+// function main begins program
+int main(){
+// a) Using one statement with two stream insertion operators.
+cout << "12 " << "34" << endl;
+
+// b) Using one statement with four stream insertion operator.
+cout << "1" << "2 " << "3" << "4" << endl;
+
+// c) Using two statements with one stream insertion operator each.
+cout << "12 ";
+cout << "34" << endl;
+
+return 0; // indicate that program ended successfully
+} // end of function main
