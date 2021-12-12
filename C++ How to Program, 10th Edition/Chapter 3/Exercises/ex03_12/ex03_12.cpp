b9b8dac535b78b6168e028c30fc7d1620424acae
W Negron <48074211+WNegron@users.noreply.github.com>
2021-12-12 04:30:49 -0500

    Update ex03_12.cpp

--- C++ How to Program, 10th Edition/Chapter 3/Exercises/ex03_12/ex03_12.cpp
+++ C++ How to Program, 10th Edition/Chapter 3/Exercises/ex03_12/ex03_12.cpp
@@ -5,7 +5,7 @@ Filename: ex03_12.cpp
   Author: Warren Negron
    Email: warren.negron@gmail.com
 
-Note: My answer to exercise 3.12 using only the tools and methods thought in Chapter 2.
+Note: My answer to exercise 3.12 using only the tools and methods thought in Chapter 3.
 
 Description:
 
