39550a2fe27a471462812f99e2366583da4ca5e9
W Negron <48074211+WNegron@users.noreply.github.com>
2021-12-12 04:39:52 -0500

    Update ex02_19.cpp

--- C++ How to Program, 10th Edition/Chapter 2/Exercises/ex02_19.cpp
+++ C++ How to Program, 10th Edition/Chapter 2/Exercises/ex02_19.cpp
@@ -5,7 +5,7 @@ Filename: ex02_19.cpp
 	Author: Warren Negron
 	Email: warren.negron@gmail.com
 
-Note: My answer to exercise 2.19 using only the tools and methods thought in Chapter 2.
+Note: My answer to exercise 2.19 using only the tools and methods taught up to Chapter 2.
 
 Description:
 2.19 (Arithmetic, Smallest and Largest) Write a program that inputs three integers
