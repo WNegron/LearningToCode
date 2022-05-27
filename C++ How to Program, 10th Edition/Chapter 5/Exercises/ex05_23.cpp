9b0627b86744f7e880a102c462d61f477f7c283e
Tenchi <48074211+WNegron@users.noreply.github.com>
2022-05-26 20:48:07 -0400

    Update ex05_23.cpp

--- C++ How to Program, 10th Edition/Chapter 5/Exercises/ex05_23.cpp
+++ C++ How to Program, 10th Edition/Chapter 5/Exercises/ex05_23.cpp
@@ -38,17 +38,7 @@ using namespace std; //program uses names from the std namespace
 int main(){
 
 // declaration and initializing variables
-int sizeOfEggTimer = 0;
-
-cout << "Egg-Timer Program: the smallest size egg-timer it can print is size 5";
-cout << "\n anything less will not look like an egg-timer \n";
- 
-while( sizeOfEggTimer != -1){	
-//prompt user for the size of the egg-timer
-cout << "Enter size of the Egg-Timer to be printed (-1 to exit): ";
-cin >> sizeOfEggTimer;//read size from user
-
-cout << endl;//print newline
+int sizeOfEggTimer = 9;
 
 for(int i = 1; i <= sizeOfEggTimer; i++){
 	
@@ -82,75 +72,38 @@ for(int i = 1; i <= sizeOfEggTimer; i++){
 				else if((j >= (sizeOfEggTimer - i)) && (j < sizeOfEggTimer))
 					cout << " ";
 			}//end of for
-		}//end of else if third line
+		}//end of else if third line to half
 		
-		//prints the middle if size is an odd number
-		else if(sizeOfEggTimer % 2 == 1){
-			if(i == (sizeOfEggTimer/2) + 1){
-				for(int j = 1; j <= sizeOfEggTimer; j++){
-					if(j == 1 || j == sizeOfEggTimer)
-						cout << "#"; 
-					else if( j == i)
-						cout << "*";
-					else if(j > 1 && j < i)
-						cout << " ";
-					else if(j > i && j < sizeOfEggTimer)
-						cout << " ";
-				}//end of for
-			}//end of if
+		//prints the middle
+		else if(i == (sizeOfEggTimer/2) + 1){
+			for(int j = 1; j <= sizeOfEggTimer; j++){
+				if(j == 1 || j == sizeOfEggTimer)
+					cout << "#"; 
+				else if( j == i)
+					cout << "*";
+				else if(j > 1 && j < i)
+					cout << " ";
+				else if(j > i && j < sizeOfEggTimer)
+					cout << " ";
+			}//end of for
+		}//end of else if middle
 			
-			//prints bottom half of the egg timer
-			else if(i > ((sizeOfEggTimer/2) + 1)){
-				for(int j = 1; j <= sizeOfEggTimer; j++){
-					if(j==1 || j==sizeOfEggTimer)
-						cout << "#";
-					else if(j > 1 && j <= (sizeOfEggTimer-i))
-						cout << " ";
-					else if(j > (sizeOfEggTimer-i) && j <= (sizeOfEggTimer - (sizeOfEggTimer-i)))
-						cout << "*";
-					else if(j > (sizeOfEggTimer - (sizeOfEggTimer-i)) && j < sizeOfEggTimer)
-						cout << " ";
-						
-				}//end of for
-			}//end of else if bottom half
-		}//end else if middle for an odd size number
-		
-		//prints the middle if size is an even number
-		else if(sizeOfEggTimer % 2 == 0){
-			if(i == (sizeOfEggTimer/2)){
-				for(int j = 1; j <= sizeOfEggTimer; j++){
-					if(j == 1 || j == sizeOfEggTimer)
-						cout << "#"; 
-					else if( j == i)
-						cout << "*";
-					else if(j > 1 && j < i)
-						cout << " ";
-					else if(j > i && j < sizeOfEggTimer)
-						cout << " ";
-				}//end of for
-			}//end of if
-			
-			//prints bottom half of the egg timer
-			else if(i > (sizeOfEggTimer/2)){
-				for(int j = 1; j <= sizeOfEggTimer; j++){
-					if(j==1 || j==sizeOfEggTimer)
-						cout << "#";
-					else if(j > 1 && j <= (sizeOfEggTimer-i))
-						cout << " ";
-					else if(j > (sizeOfEggTimer-i) && j <= (sizeOfEggTimer - (sizeOfEggTimer-i)))
-						cout << "*";
-					else if(j > (sizeOfEggTimer - (sizeOfEggTimer-i)) && j < sizeOfEggTimer)
-						cout << " ";
-				}//end of for
-			}//end of else if bottom half 
-		}//end else if middle for an even size number
-
+		//prints bottom half of the egg timer
+		else if(i > ((sizeOfEggTimer/2) + 1)){
+			for(int j = 1; j <= sizeOfEggTimer; j++){
+				if(j==1 || j==sizeOfEggTimer)
+					cout << "#";
+				else if(j > 1 && j <= (sizeOfEggTimer-i))
+					cout << " ";
+				else if(j > (sizeOfEggTimer-i) && j <= (sizeOfEggTimer - (sizeOfEggTimer-i)))
+					cout << "*";
+				else if(j > (sizeOfEggTimer - (sizeOfEggTimer-i)) && j < sizeOfEggTimer)
+					cout << " ";
+			}//end of for
+		}//end of else if bottom half
 		
 	cout << endl;//print newline
 }//end of for int i 
-cout << endl;//print newline
-
-}//end of while loop
 
 return 0; // indicate that program ended successfully
 } // end of function main
