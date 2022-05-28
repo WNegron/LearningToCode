1c1eebb5cf93956340116bdcc8fe0e4f36722137
Tenchi <48074211+WNegron@users.noreply.github.com>
2022-05-28 19:24:58 -0400

    Create ex02_21.c

---
+++ C How to Program, 8th Edition/Chapter 02/Exercises/ex02_21.c
@@ -0,0 +1,49 @@
+/***************************************************************************************************
+File Name: ex02_21.c
+     Date: 2022-05-28
+   Author: Warren Negron
+    Email: warren.negron@gmail.com
+   
+Notes:
+My answer to exercise 2.21 using only the tools and methods taught up to chapter 2.
+
+Description:
+	2.21 (Shapes with Asterisks) Write a program that prints the following shapes with asterisks.
+
+--------- Example Output --------------------------------------------------------------
+*********     ***       *        *
+*       *    *   *     ***      * *
+*       *   *     *   *****    *   *
+*       *   *     *     *     *     *
+*       *   *     *     *    *       *
+*       *   *     *     *     *     *
+*       *   *     *     *      *   *   
+*       *    *   *      *       * *     
+*********     ***       *        *         
+---------------------------------------------------------------------------------------
+
+   
+-----------------------------      Pseudocode      ------------------------------------
+
+-----------------------------    End Pseudocode    ------------------------------------
+
+***************************************************************************************************/
+
+#include <stdio.h>//standard input/output header
+
+//function main begins program execution
+int main(void){
+	//variable definition
+	
+	puts("*********     ***       *        *");
+	puts("*       *    *   *     ***      * *");
+	puts("*       *   *     *   *****    *   *");
+	puts("*       *   *     *     *     *     *");
+	puts("*       *   *     *     *    *       *");
+	puts("*       *   *     *     *     *     *");
+	puts("*       *   *     *     *      *   *");
+  puts("*       *    *   *      *       * *");
+  puts("*********     ***       *        *");
+
+}//end function main
+
