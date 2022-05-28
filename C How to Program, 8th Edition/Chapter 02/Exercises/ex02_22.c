7d02ff0fd377b30795f1bc396b6bcd844eeb8893
Tenchi <48074211+WNegron@users.noreply.github.com>
2022-05-28 19:34:33 -0400

    Create ex02_22.c

---
+++ C How to Program, 8th Edition/Chapter 02/Exercises/ex02_22.c
@@ -0,0 +1,38 @@
+/***************************************************************************************************
+File Name: ex02_22.c
+     Date: 2022-05-22
+   Author: Warren Negron
+    Email: warren.negron@gmail.com
+   
+Notes:
+My answer to exercise 2.22 using only the tools and methods taught up to chapter 2.
+
+Description:
+	2.22 What does the following code print? 
+		
+		printf( "*\n**\n***\n****\n*****\n" );
+
+--------- Example Output --------------------------------------------------------------
+  *
+  **
+  ***
+  ****
+  *****
+---------------------------------------------------------------------------------------
+
+   
+-----------------------------      Pseudocode      ------------------------------------
+
+-----------------------------    End Pseudocode    ------------------------------------
+
+***************************************************************************************************/
+
+#include <stdio.h>//standard input/output header
+
+//function main begins program execution
+int main(void){
+	//variable definition
+	
+	printf( "*\n**\n***\n****\n*****\n" );
+}//end function main
+
