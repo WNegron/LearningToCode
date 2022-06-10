29d9d14d74505e80e64c8e9726a5f413d561c965
W Negron <tenchi.skywalker@outlook.com>
2022-06-09 20:51:25 -0400

    firs

---
+++ C How to Program, 8th Edition/Chapter 3/Exercises/ex03_13.c
@@ -0,0 +1,33 @@
+/***************************************************************************************************
+File Name: filename.c
+     Date: yyyy-mm-dd
+   Author: Warren Negron
+    Email: warren.negron@gmail.com
+   
+Notes:
+My answer to exercise x.xx using only the tools and methods taught up to chapter xx.
+
+Description:
+For Exercises 3.16–3.20, perform each of these steps:
+1. Read the problem statement.
+2. Formulate the algorithm using pseudocode and top-down, stepwise refinement. 3. Write a C program.
+4. Test, debug and execute the C program.
+
+----------   Example Output   ----------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------------------
+
+   
+-------------------------------------    Pseudocode    ---------------------------------------------
+
+-----------------------------------    End Pseudocode    -------------------------------------------
+
+***************************************************************************************************/
+
+#include <stdio.h>//standard input/output header
+
+//function main begins program execution
+int main(void){
+	//variable definition
+	
+}//end function main
