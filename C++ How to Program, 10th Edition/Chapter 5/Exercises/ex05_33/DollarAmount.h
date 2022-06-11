f71cf8bb85ddea0593e5181fe65bf1f62714122f
W Negron <tenchi.skywalker@outlook.com>
2022-06-11 02:02:36 -0400

    Updated ex05_32

--- C++ How to Program, 10th Edition/Chapter 5/Exercises/ex05_33/DollarAmount.h
+++ C++ How to Program, 10th Edition/Chapter 5/Exercises/ex05_33/DollarAmount.h
@@ -1,25 +1,20 @@
 /***************************************************************************************************
 
 File Name: DollarAmount.h
-     Date: 2022-06-10
+     Date: 2022-06-11
    Author: Warren Negron
     Email: warren.negron@gmail.com
 
 Note: 
-My answer to exercise 5.32 using only the tools and methods taught up to Chapter 5.
+My answer to exercise 5.33 using only the tools and methods taught up to Chapter 5.
 
 This exercise is from the global Edition of C++ How to Program, 10/e 
 
 Description:
 	
-	5.32 (DollarAmount with Banker’s Rounding) The DollarAmount class’s addInterest member function 
-uses the biased half-up rounding technique in which fractional amounts of .1, .2, .3 and .4 round 
-down, and .5, .6, .7, .8 and .9 round up. In this technique, four values round down and five round 
-up. Banker’s rounding fixes this problem by rounding .5 to the nearest even integer—e.g., 0.5 rounds 
-to 0, 1.5 and 2.5 round to 2, 3.5 and 4.5 round to 4, etc. Enhance class DollarAmount from Exercise 
-5.31 by reimplementing addInterest to use banker’s rounding, then retest the compound-interest 
-program.
-	
+	5.33 (DollarAmount with dollars and cents Data Members) Reimplement class DollarAmount from 
+Exercise 5.32 to store data members dollars and cents, rather than amount. Modify the body of each 
+constructor and member function appropriately to manipulate the dollars and cents data members.
 ***************************************************************************************************/
 #include <cmath> //enables program to use mathematical functions
 #include <string> // enable program to use C++ string data type
@@ -64,17 +59,12 @@ void addInterest(int rate, int divisor){
 	int interestCents = ((amount * rate) / divisor) % 100;
 	
 	if(interestCents >= 50 && interestCents < 60){
-		if(((interestDollars % 10) % 2 ) == 1){//verifies if interestDollars is an odd number
-			++interestDollars;//round to the next even number
-			interestCents = 0;//round to zero
+		if(((interestDollars % 10) % 2 ) == 1){
+			++interestDollars;
+			interestCents = 0;
+			
 			DollarAmount interest{interestDollars,interestCents};
-			 interest.toPennies();
-			 add(interest);// add interest to this object's amount
-		}//end if
-		else if(((interestDollars % 10) % 2 ) == 0){//verifies if interestDollar is an even number
-			//interestDollars stays as the even number; only cents need to be rounded to zero
-			interestCents = 0;//round to zero
-			DollarAmount interest{interestDollars,interestCents};
+
 			 interest.toPennies();
 			 add(interest);// add interest to this object's amount
 		}//end if
