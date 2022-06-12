635b139a0f1a312adb1bb070adaf912e655aa7c1
W Negron <tenchi.skywalker@outlook.com>
2022-06-12 05:16:11 -0400

    Update ex04_15.c

--- C How to Program, 8th Edition/Chapter 4/Exercises/ex04_15.c
+++ C How to Program, 8th Edition/Chapter 4/Exercises/ex04_15.c
@@ -22,14 +22,20 @@ int main(void){
     double principal = 1000.0; // starting principal
     double rate = .05; // annual interest rate
     
-    // output table column heads
-    printf("%4s%21s\n", "Year", "Amount on deposit");
+   
 
-    // calculate amount on deposit for each of ten years
-    for (unsigned int year = 1; year <= 10; ++year) {
-        // calculate new amount for specified year
-        double amount = principal * pow(1.0 + rate, year);
-        // output one table row
-        printf("%4u%21.2f\n", year, amount);
-    }//end for	
+    //vary the interest rate
+    for(int i = 5; i <= 10; i++){
+        rate = i / 100.00;
+        // output table column heads
+        printf("\n%4s %21s %4s %.2f\n", "Year", "Amount on deposit","Interest Rate: ",rate);
+        
+        // calculate amount on deposit for each of ten years
+        for (unsigned int year = 1; year <= 10; ++year) {
+            // calculate new amount for specified year
+            double amount = principal * pow(1.0 + rate, year);
+            // output one table row
+            printf("%4u%21.2f\n", year, amount);
+        }//end for	
+    }//end for
 }//end function main
