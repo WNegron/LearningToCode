5cac999b445fd52474d18f4835ed7b9b6e5c9c51
W Negron <tenchi.skywalker@outlook.com>
2022-07-05 05:10:51 -0400

    updated ex05_08.c

--- C How to Program, 8th Edition/Chapter 5/Exercise/ex05_08.c
+++ C How to Program, 8th Edition/Chapter 5/Exercise/ex05_08.c
@@ -40,13 +40,30 @@ int main(void){
 
      //a) x = fabs(7.5);
      x = fabs(7.5);
-     printf("\"x = fabs(7.5)\" = %.6f \n", x);
+     printf("\n\"x = fabs(7.5)\" = %.6f \n", x);
 
      //b) x = floor(7.5);
+     x = floor(7.5);
+     printf("\"x = floor(7.5)\" = %.6f \n", x);
+
      //c) x = fabs(0.0);
+     x = fabs(0.0);
+     printf("\"x = fabs(0.0)\" = %.6f \n", x);
+
      //d) x = ceil(0.0);
+     x = ceil(0.0);
+     printf("\"x = ceil(0.0)\" = %.6f \n", x);
+
      //e) x = fabs(-6.4);
+     x = fabs(-6.4);
+     printf("\"x = fabs(-6.4)\" = %.6f \n", x);
+
      //f) x = ceil(-6.4);
+     x = ceil(-6.4);
+     printf("\"x = ceil(-6.4)\" = %.6f \n", x);
+
      //g) x = ceil(-fabs(-8 + floor(-5.5)));
+     x = ceil(-fabs(-8 + floor(-5.5)));
+     printf("\"x = ceil(-fabs(-8 + floor(-5.5)))\" = %.6f \n", x);
 	
 }//end function main
