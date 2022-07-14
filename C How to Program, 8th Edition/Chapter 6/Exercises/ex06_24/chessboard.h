ed27b4a161973aef9b25c7b1e50ac7aa22ac4eba
W Negron <tenchi.skywalker@outlook.com>
2022-07-13 22:02:01 -0400

    Updated ex06_24.c

---
+++ C How to Program, 8th Edition/Chapter 6/Exercises/ex06_24/chessboard.h
@@ -0,0 +1,43 @@
+/***************************************************************************************************
+File Name: chessboard.h
+     Date: 2022-07-13
+   Author: Warren Negron
+    Email: warren.negron@gmail.com
+   
+Notes:
+My answer to exercise 6.11 using only the tools and methods taught up to chapter 6.
+
+Description:
+
+***************************************************************************************************/
+//symbolic constant
+#define ROWS 8
+#define COLUMNS 8
+
+//function to initialize the chessboard; so the array is not full of garbage
+void initializeBoard(int chessboard[ROWS][COLUMNS],int initialRow, int initialColumn){
+    for(int row = 0; row < ROWS; row++){
+        for(int column = 0; column < COLUMNS; column++){
+            if((row == initialRow) && (column == initialColumn)){
+                chessboard[row][column]='K';
+            }//end if
+            else{
+                chessboard[row][column]=' ';
+            }//end else
+        }//end for loop 'column'
+    }//end for loop 'row'
+}//end function initializedBoard
+
+
+void printBoard(int chessboard[ROWS][COLUMNS]){ //,int rowSize, int columnSize){
+    printf("%s","    0   1   2   3   4   5   6   7\n");
+        puts("  ---------------------------------");
+        for(int row = 0; row < ROWS; row++){
+            printf("%d |",row);
+            for(int column = 0; column < COLUMNS; column++){
+                printf(" %c |", chessboard[row][column]);
+            }//end for loop 'column'
+            puts("");
+            puts("  ---------------------------------");
+        }//end for loop 'rows'
+}//end function printBoard
