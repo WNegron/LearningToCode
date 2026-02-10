b89e97ef5e89d52b989fb11efbfe831f72d6a632
Warren <tenchi.skywalker@outlook.com>
2026-02-10 01:27:17 -0500

    update

---
+++ C How to Program, 8th Edition/Chapter 5/Exercises/ex05_27/ex05_27.c
@@ -0,0 +1,32 @@
+/***************************************************************************************************
+File Name: ex05_27.c
+     Date: 2026-02-10
+   Author: Warren Negron
+    Email: warren.negron@gmail.com
+   
+Notes:
+My answer to exercise 5.27 using only the tools and methods taught up to chapter 5.
+
+Description:
+	5.27 (Prime Numbers) An integer is said to be prime if it’s divisible by only 1 and itself. For 
+example, 2, 3, 5 and 7 are prime, but 4, 6, 8 and 9 are not.
+
+    a) Write a function that determines whether a number is prime.
+
+    b) Use this function in a program that determines and prints all the prime numbers between 1 and 
+    10,000. How many of these 10,000 numbers do you really have to test before being sure that you 
+    have found all the primes?
+
+    c) Initially you might think that n/2 is the upper limit for which you must test to see whether 
+    a number is prime, but you need go only as high as the square root of n. Rewrite the program, 
+    and run it both ways. Estimate the performance improvement.
+
+***************************************************************************************************/
+
+#include <stdio.h>//standard input/output header
+
+//function main begins program execution
+int main(void){
+	//variable definition
+	
+}//end function main
---
+++ C How to Program, 8th Edition/template.c
@@ -0,0 +1,62 @@
+/***************************************************************************************************
+File Name: filename.c
+     Date: yyyy-mm-dd
+   Author: Warren Negron
+    Email: warren.negron@gmail.com
+   
+Notes:
+My answer to exercise x.xx using only the tools and methods taught up to chapter xx.
+
+Description:
+	
+
+----------   Example Output   ----------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------------------
+
+   
+-------------------------------------    Pseudocode    ---------------------------------------------
+
+-----------------------------------    End Pseudocode    -------------------------------------------
+
+***************************************************************************************************/
+
+#include <stdio.h>//standard input/output header
+
+//function main begins program execution
+int main(void){
+	//variable definition
+	
+}//end function main
+
+/*
+<assert.h> Contains information for adding diagnostics that aid program debugging.
+
+<ctype.h> Contains function prototypes for functions that test characters for certain properties, and function prototypes for functions that can be used to convert lowercase letters to uppercase letters and vice versa.
+
+<errno.h> Defines macros that are useful for reporting error conditions.
+
+<float.h> Contains the floating-point size limits of the system.
+
+<limits.h> Contains the integral size limits of the system.
+
+<locale.h> Contains function prototypes and other information that enables a program to be modified for the current locale on which it’s running. The notion of locale enables the computer system to handle different conventions for expressing data such as dates, times, currency amounts and large numbers throughout the world.
+
+<math.h> Contains function prototypes for math library functions.
+
+<setjmp.h> Contains function prototypes for functions that allow bypassing of the usual function call and return sequence.
+
+<signal.h> Contains function prototypes and macros to handle various conditions that may arise during program execution.
+
+<stdarg.h> Defines macros for dealing with a list of arguments to a function whose number and types are unknown.
+
+<stddef.h> Contains common type definitions used by C for performing calculations.
+
+<stdio.h> Contains function prototypes for the standard input/output library functions, and information used by them.
+
+<stdlib.h> Contains function prototypes for conversions of numbers to text and text to numbers, memory allocation, random numbers and other utility functions.
+
+<string.h> Contains function prototypes for string-processing functions.
+
+<time.h> Contains function prototypes and types for manipulating the time and date.
+*/
