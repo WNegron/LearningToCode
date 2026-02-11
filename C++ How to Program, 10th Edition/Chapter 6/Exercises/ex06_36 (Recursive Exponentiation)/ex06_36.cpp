ec132d2b0acd313f564d41e4e0afedf8ecadd2bb
Warren <tenchi.skywalker@outlook.com>
2026-02-11 02:01:55 -0500

    update

---
+++ C++ How to Program, 10th Edition/Chapter 6/Exercises/ex06_36 (Recursive Exponentiation)/ex06_36.cpp
@@ -0,0 +1,94 @@
+/***************************************************************************************************
+
+File Name: ex06_36.cpp
+     Date: 2026-02-11
+   Author: Warren Negron
+    Email: warren.negron@gmail.com
+
+Note: 
+My answer to exercise 6.36 using only the tools and methods taught up to Chapter 6.
+
+This exercise is from the global Edition of C++ How to Program, 10/e 
+
+Description:
+    6.36 (Recursive Exponentiation) Write a recursive function power(base, exponent) that, when
+    invoked, returns
+        
+        base exponent
+
+    For example, power(3, 4) = 3 * 3 * 3 * 3. Assume that exponent is an integer greater than or equal
+to 1. Hint: The recursion step would use the relationship
+        
+        base exponent = base · base exponent - 1
+
+    and the terminating condition occurs when exponent is equal to 1, because
+        
+        base1 = base
+
+-------------------------------------    Pseudocode    ---------------------------------------------
+
+main function
+    base
+    exponent
+    result
+
+    display "Enter base integer:"
+    reads base
+
+    display "Enter exponent integer:"
+    reads exponent
+
+    result = power(base,integer)
+
+    display base, " to the power of ", exponent, " = ", result
+end main
+
+funciton power(base, exponent)
+    if exponent = 0 then
+        return 1
+    if exponent = 1 then
+        return base
+    else
+        return base * power(base,exponent -1)
+end power function
+-----------------------------------    End Pseudocode    -------------------------------------------
+    
+***************************************************************************************************/
+#include <iostream>//enable program to perform input/output
+#include <ctime>//enable program to perform time functions
+#include <iomanip>//parameterized stream manipulators
+#include <cmath>//enable program to perform math functions
+#include <cstdlib> // contains prototypes for functions srand and rand
+#include <random> // contains C++11 random number generation features
+#include <utility>//enables program to use pair
+
+#include "recursiveExponentiation.hpp"//enable program to use function from recursiveExponentiation
+
+using namespace std;//program uses names from namespace std
+
+// function main begins program execution
+int main(){
+    // declaring and initializing variables
+    int base = 0;
+    int exponent = 0;
+    //int result = 0;
+
+    while(true){//forever loop
+
+    //promtp user to enter a base
+    std::cout << "Enter base integer: ";
+    std::cin >> base;
+
+    //prompt user to enter an exponent
+    std::cout << "Enter exponent integer: ";
+    std::cin >> exponent;
+
+    //result = power(base,exponent);//call power function
+
+    //displays the result
+    std::cout << base << "^" << exponent << " = " << power(base,exponent) << std::endl;
+    }//end of while loop
+
+    return 0;// indicate that program ended successfully
+}// end of main function
+
---
+++ C++ How to Program, 10th Edition/Chapter 6/Exercises/ex06_36 (Recursive Exponentiation)/recursiveExponentiation.hpp
@@ -0,0 +1,69 @@
+/***************************************************************************************************
+
+File Name: recursiveExponentiation.hpp
+     Date: 2026-02-11
+   Author: Warren Negron
+    Email: warren.negron@gmail.com
+
+Note: 
+My answer to exercise 6.36 using only the tools and methods taught up to Chapter 6.
+
+This exercise is from the global Edition of C++ How to Program, 10/e 
+
+Description:
+    6.36 (Recursive Exponentiation) Write a recursive function power(base, exponent) that, when
+    invoked, returns
+        
+        base exponent
+
+    For example, power(3, 4) = 3 * 3 * 3 * 3. Assume that exponent is an integer greater than or equal
+to 1. Hint: The recursion step would use the relationship
+        
+        base exponent = base · base exponent - 1
+
+    and the terminating condition occurs when exponent is equal to 1, because
+        
+        base1 = base
+
+-------------------------------------    Pseudocode    ---------------------------------------------
+
+main function
+    base
+    exponent
+    result
+
+    display "Enter base integer:"
+    reads base
+
+    display "Enter exponent integer:"
+    reads exponent
+
+    result = power(base,integer)
+
+    display base, " to the power of ", exponent, " = ", result
+end main
+
+funciton power(base, exponent)
+    if exponent = 0 then
+        return 1
+    if exponent = 1 then
+        return base
+    else
+        return base * power(base,exponent -1)
+end power function
+-----------------------------------    End Pseudocode    -------------------------------------------
+    
+***************************************************************************************************/
+
+//power function begins
+int power(int base, int exponent){
+    if(exponent == 0){
+        return 1;
+    }//end if
+    if (exponent == 1){
+        return base;
+    }//end if
+    else{
+        return base * power(base,exponent -1);
+    }
+}//end power function
