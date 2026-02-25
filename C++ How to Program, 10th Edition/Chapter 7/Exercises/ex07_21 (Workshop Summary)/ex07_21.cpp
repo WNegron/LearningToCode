db186dae4a0ab675f48522a908a5743c22e15f5a
Warren <tenchi.skywalker@outlook.com>
2026-02-25 05:46:02 -0500

    update

---
+++ C++ How to Program, 10th Edition/Chapter 7/Exercises/ex07_21 (Workshop Summary)/ex07_21.cpp
@@ -0,0 +1,50 @@
+/***************************************************************************************************
+
+File Name: exXX_XX.cpp
+     Date: XXXX-XX-XX
+   Author: Warren Negron
+    Email: warren.negron@gmail.com
+
+Note: 
+My answer to exercise X.XX using only the tools and methods taught up to Chapter X.
+
+This exercise is from the global Edition of C++ How to Program, 10/e 
+
+Description:
+
+7.21 (Workshop Summary) Use a two-dimensional array to solve the following problem. A craft
+workshop has three craftspeople (1 to 3) who produce four different products (1 to 4). Once a day,
+each craftsperson passes in a slip for each different type of product completed. Each slip contains
+the following:
+a) The craft-person’s number
+b) The product number
+c) The quantity of items completed
+Thus, each craftsperson passes in between 0 and 4 production slips per day. Write a program that
+will read in the information as supplied on each individual production slip and summarize the total
+items manufactured by a craftsperson by product. All totals should be stored in the two-dimensional
+array stock. After processing all the information for the last month, print the results in tabular
+format with each column representing a particular product and each row representing a particular
+craftsperson. Total across each row to get the total quantity manufactured by each craftsperson for
+last month; total across each column to get the total quantity manufactured of each product for last
+month. Display all this information in a tabular format.
+
+-------------------------------------    Pseudocode    ---------------------------------------------
+
+-----------------------------------    End Pseudocode    -------------------------------------------
+    
+***************************************************************************************************/
+
+//Standard Library Header Files
+#include <iostream>//enable program to perform input/output
+#include <array>//contains the definition of class template array
+
+//using directives; enables a program to use all the names in any standard C++ header
+using namespace std;//program uses names from namespace std
+
+// function main begins program execution
+int main(){
+    // declaring and initializing variables
+
+    return 0;// indicate that program ended successfully
+}// end of main function
+
